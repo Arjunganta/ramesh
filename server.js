737b3c908dd75a7c11dd12731b8da107b37fc4dc
Arjun Ganta <arjunganta007@gmail.com>
2021-09-08 01:33:29 +0530

    Reverting changes in server.js

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory;
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//something
